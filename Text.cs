ceabd314f21ad7c1b561b9d52ff2da2e01d00b11
Lee Hoon Lim <hoonlim123@gmail.com>
2023-08-14 14:27:16 +0800

    Initial

---
+++ Text.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class Text
+    {
+        public Vector2 Position = null;
+        public string TextString;
+        public Font Font;
+
+        public Text(string TextString, Font Font, Vector2 Position)
+        {
+            this.TextString = TextString;
+            this.Font = Font;
+            this.Position = Position;
+
+            GameEngine.RegisterText(this);
+        }
+        public void DestroySelf()
+        {
+            GameEngine.UnRegisterText(this);
+        }
+    }
+}
